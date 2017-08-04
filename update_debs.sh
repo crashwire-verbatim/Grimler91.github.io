330388487a4dbe911435e38496a0c146c91c645a
Grimler91 <HenrikGrimler.se>
2017-08-04 11:32:41 +0000

    Add correct InRelease and update_debs.sh

---
+++ update_debs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo Status: 200 Okay
+echo Content-Type: text/plain
+echo
+echo Rebuilding APT repository:
+
+{
+  #-- settings
+  export KEYNAME=9B4E7D27395024EA5A4FC6395AAAC9E0A46BE53C
+  cd files
+
+  for folder in dists/testing/{texlive,x}/{binary-arm,binary-all}; do
+    if [ -d "$folder" ]; then
+      apt-ftparchive packages $folder > $folder/Packages
+      bzip2 -kf $folder/Packages
+    fi
+  done
+
+  #-- signed Release file
+  apt-ftparchive -c ../testing.conf release dists/testing/ > dists/testing/Release
+  gpg2 --yes -abs -u $KEYNAME -o dists/testing/Release.gpg dists/testing/Release
+  gpg2 --yes --clearsign -u $KEYNAME -o dists/testing/InRelease dists/testing/Release
+  cd ..
+} 2>&1
